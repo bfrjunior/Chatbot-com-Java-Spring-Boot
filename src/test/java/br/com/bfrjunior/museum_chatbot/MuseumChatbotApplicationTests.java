7389622cecda8d09b517ef008a3209ef357bf25b
Fernando Rocha <beneditofernandor@gmail.com>
2024-08-19 23:06:53 -0300

    feat: initial commit

---
+++ src/test/java/br/com/bfrjunior/museum_chatbot/MuseumChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bfrjunior.museum_chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuseumChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
